c2be124f34796ac5f8a682b858288ff887600d6a
PraveenSahana <PraveenSahana@LAPTOP-J7HPDAO2>
2022-05-14 08:45:10 +0530

    Add project files.

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("data source=.; initial catalog=ReactFullStack;Integrated Security=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ DAL/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAL
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+    }
+}
